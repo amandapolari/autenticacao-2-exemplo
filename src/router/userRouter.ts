341f0c008bcb54e68c1aced10e1b660c0585b9fe
Amanda <amandaoliveirapolari@gmail.com>
2023-11-13 16:46:40 -0300

    implementacao bcryptjs

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -4,11 +4,17 @@ import { UserController } from '../controller/UserController';
 import { UserDatabase } from '../database/UserDatabase';
 import { IdGenerator } from '../services/IdGenerator';
 import { TokenManager } from '../services/TokenManager';
+import { HashManager } from '../services/HashManager';
 
 export const userRouter = express.Router();
 
 const userController = new UserController(
-    new UserBusiness(new UserDatabase(), new IdGenerator(), new TokenManager())
+    new UserBusiness(
+        new UserDatabase(),
+        new IdGenerator(),
+        new TokenManager(),
+        new HashManager()
+    )
 );
 
 userRouter.get('/', userController.getUsers);
